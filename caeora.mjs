27c99b54abede2a0f1b27c7fde325998a98b60a6
Jakob Goebel <jakob.goebel@avenga.com>
2022-11-09 16:47:22 +0100

    Add setting and logic to handle remote module locations

--- caeora.mjs
+++ caeora.mjs
@@ -1,8 +1,14 @@
 // The base token path
 const TOKEN_PATH = "modules/caeora-maps-tokens-assets/assets/tokens/";
 
+// Store a reference to a special location of the token path (e.g. a Forge Assets Library)
+let tokenPathLocation = "";
+
 // A cached list of available tokens
 let availableTokens = new Set();
+
+// A cached list of available remote tokens to reduce network requests
+let availableRemoteTokens = new Map();
 
 // Store a reference to whether artwork is being replaced
 let replaceArtwork = false;
@@ -25,41 +31,98 @@ function initialize() {
 		default: false,
 		onChange: replace => replaceArtwork = replace
 	});
-	
-	// Assign the current saved value if there is one
-        replaceArtwork = game.settings.get("caeora-maps-tokens-assets", "replaceArtwork") ?? false;
+
+	// Register token replacement setting
+	game.settings.register("caeora-maps-tokens-assets", "tokenPathLocation", {
+		name: "Location of the module",
+		hint: "If you store the module in a location other than User Data (for example in a Forge Assets Library), you need to specify that location here. Example: https://assets.forge-vtt.com/1234567890abcdefghijklmn/",
+		scope: "world",
+		config: true,
+		type: String,
+		default: "",
+		onChange: setTokenPath,
+	});
+
+	// Assign the current saved values if there are any
+	replaceArtwork = game.settings.get("caeora-maps-tokens-assets", "replaceArtwork") ?? false;
+	setTokenPath(game.settings.get("caeora-maps-tokens-assets", "tokenPathLocation") ?? "");
 
 	// Handle actor replacement, if the setting is enabled
 	Hooks.on("preCreateActor", replaceActorArtwork);
+}
 
-	// Cache available tokens
-	cacheAvailableTokens();
+function setTokenPath(location) {
+	tokenPathLocation = location ?? "";
+
+	// Make sure to cache available local tokens if switching back to "local mode"
+	if (!tokenPathLocation) {
+		cacheAvailableTokens();
+	}
 }
 
 /**
- * Cache the set of available tokens which can be used to replace artwork to avoid repeated filesystem requests
+ * Cache the set of available tokens which can be used to replace artwork to avoid repeated filesystem requests.
+ * Will not be used if tokens are stored remotely.
  */
 async function cacheAvailableTokens() {
 	availableTokens.clear();
 	const crs = await FilePicker.browse("data", TOKEN_PATH);
 	for ( let cr of crs.dirs ) {
-		const tokens = await FilePicker.browse("data", cr+"/with-shadows/");
-		tokens.files.forEach(t => availableTokens.add(t));
+		try {
+			const tokens = await FilePicker.browse("data", cr+"/with-shadows/");
+			tokens.files.forEach(t => availableTokens.add(t));
+		} catch {}
+	}
+}
+
+/**
+ * Verifies the given token does exist in the expected location
+ * @param {String} src The token source
+ */
+function sourceExists(src) {
+	// If the tokens are stored locally, we only need to check available tokens
+	if ( !tokenPathLocation) {
+		return availableTokens.has(src);
+	}
+
+	// If the tokens are stored remotely, we first check our cache
+	if (availableRemoteTokens.has(src)) {
+		return availableRemoteTokens.get(src)
+	}
+
+	try {
+		const request = new XMLHttpRequest();
+		request.open("HEAD", src, false);
+		request.send();
+
+		// Check if the token exists at the expected location
+		const exists = request.status === 200;
+		// Cache the result
+		availableRemoteTokens.set(src, exists)
+
+		return exists
+	} catch {
+		// In case of any errors, we assume the token does not exist but don't cache the result
+		return false;
 	}
 }
 
 /**
  * Replace the artwork for a NPC actor with the version from this module
  */
-function replaceActorArtwork(data, options, userId) {
-	if ( !replaceArtwork || (data.type !== "npc") || !hasProperty(data, "data.details.cr") ) return;
-	const cleanName = data.name.replace(/ /g, "");
-	const crDir = String(getProperty(data, "data.details.cr")).replace(".", "-");
-	const tokenSrc = `${TOKEN_PATH}cr${crDir}/with-shadows/${cleanName}.png`;
-	if ( !availableTokens.has(tokenSrc) ) return;
-	data.img = tokenSrc;
-	data.token = data.token || {};
-	data.token.img = tokenSrc;
+async function replaceActorArtwork(actor, data, options, userId) {
+	if ( !replaceArtwork || (actor.type !== "npc") || !hasProperty(actor, "system.details.cr") ) return;
+
+	const cleanName = actor.name.replace(/ /g, "");
+	const crDir = String(getProperty(actor, "system.details.cr")).replace(".", "-");
+	const tokenRootPath = tokenPathLocation
+		? `${tokenPathLocation.replace(/^(.*?)\/$/, "$1")}/${TOKEN_PATH}`
+		: TOKEN_PATH;
+	const tokenSrc = `${tokenRootPath}cr${crDir}/with-shadows/${cleanName}.png`;
+
+	if (!sourceExists(tokenSrc)) return;
+
+	actor.updateSource({"img": tokenSrc, "prototypeToken.texture.src": tokenSrc});
 }
 
 // Initialize module
